4d85ef750d09ada6a86cec9dc9b4fcd74132258b
Sun Yuxi (IFAG DES SDF FW) <Yuxi.Sun@infineon.com>
2018-06-28 08:33:18 +0200

    changed conflicting naming RTC

--- examples/device-control/SleepModeXMC4700/src/SleepModeXMC4700.ino
+++ examples/device-control/SleepModeXMC4700/src/SleepModeXMC4700.ino
@@ -11,11 +11,14 @@
 #include <xmc_gpio.h>
 
 // Create an RTC object
- RTCClass RTC;
+ RTCClass rtc;
 
 // Create an DeviceControlXMC object
  XMCClass devCtrl;
-
+ 
+ void alarmMatch( );
+ void timer_cb( void );
+ 
 // Change these values to set the current initial time
  const byte seconds = 0;
  const byte minutes = 0;
@@ -33,20 +36,20 @@
 void setup( )
 {
   Serial.begin( 9600 );
-  RTC.begin();
+  rtc.begin();
   devCtrl.configureSleepMode( SLEEP_MODE, SYSCLK_FOFI, USB_OFF, SDMMC_OFF, ETH_OFF,
                                 EBU_OFF, CCU_ON, WDT_ON, FLASH_ON, PLL_ON, VCO_ON );
   pinMode( LED2, OUTPUT );
 
   TIMER_Init( 0 );  // Timer init, used to toggle LED2
 
-  RTC.setTime( hours, minutes, seconds );
-  RTC.setDate( day, month, year );
+  rtc.setTime( hours, minutes, seconds );
+  rtc.setDate( day, month, year );
 
   //Set alarm to go to sleep
-  RTC.setAlarmTime( 16, 0, 5 );
-  RTC.enableAlarm( RTC.ALARM );
-  RTC.attachInterrupt( alarmMatch );
+  rtc.setAlarmTime( 16, 0, 5 );
+  rtc.enableAlarm( rtc.ALARM );
+  rtc.attachInterrupt( alarmMatch );
 }
 
 void loop( )
@@ -54,7 +57,7 @@ void loop( )
   TIMER_Start( 8000000U );  // Start a timer
 
   if( state == 1 ){
-    RTC.setAlarmTime( 16, 0, 20 );  // Time to wake up
+    rtc.setAlarmTime( 16, 0, 20 );  // Time to wake up
     state = 2;            // Flag to wake up from sleep mode
     Serial.println( "Entering sleep mode" );
     delay( 100 );
